54976f4294e92b3cdee458d25cc4bb26e640f188
CTW121 <t.w.chua1985@gmail.com>
2025-06-22 15:06:32 +0200

    add figure of API Endpoint

--- chatbot/serializers.py
+++ chatbot/serializers.py
@@ -4,5 +4,6 @@ from .models import Conversation
 class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
-        fields = ['user_input', 'bot_response', 'is_vegetarian', 'created_at']
-        # fields = ['user_input', 'bot_response']
+        # fields = ['user_input', 'bot_response', 'is_vegetarian', 'created_at']
+        # fields = ['user_input', 'bot_response', 'created_at']
+        fields = ['user_input', 'bot_response']
